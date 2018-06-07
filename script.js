712a00e2a87fe0f74b13d6e649f41dfd8c6c10ab
ModernGabbar <moderngabbar@gmail.com>
2018-06-07 23:57:57 +0530

    new random button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let random = document.getElementById("random");
+let colors = '0123456789ABCDEF';
 
 function setGradient() {
     body.style.background =
@@ -13,6 +15,21 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function genRandColor() {
+    let color = '#';
+    for(let i=0; i<6; i++) {
+        color += colors[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", function() {
+    color1.value = genRandColor();
+    color2.value = genRandColor();
+    random.style.background = genRandColor();
+    setGradient();
+});
